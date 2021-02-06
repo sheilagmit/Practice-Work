9aef299291a18d55c9e44cab07ae92cd63896794
Sheila Bambrick <G00398264@gmit.ie>
2021-02-06 18:28:08 +0000

    First variable practice

---
+++ First Variable.py
@@ -0,0 +1,29 @@
+# Following week 03 lecture
+
+# Interger
+age = 32
+print (type(age))
+
+# List
+age = []
+print (type(age))
+
+# Boolean
+age = True
+print (type(age))
+
+# String
+age = 'hello'
+print (type(age))
+
+# String
+age = "hello"
+print (type(age))
+
+# Dict
+age = {}
+print (type(age))
+
+# Tuple
+age = ()
+print (type(age))
