9727fb4389d1dfa4f3360b7b546dee6cd4c34321
Sheila Bambrick <G00398264@gmit.ie>
2021-02-07 11:15:35 +0000

    adding subtraction program

---
+++ sub.py
@@ -0,0 +1,8 @@
+# Program to subtract on number from another.
+# input reads in a string so we need to convert it into an int so we can perform mathematical operations
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number"))
+answer= x-y
+print("{} minus {} is {}".format(x, y, answer))
+
