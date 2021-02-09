fd72190501ec35a7df1095924d878da5210a7b26
Sheila Bambrick <G00398264@gmit.ie>
2021-02-09 15:19:23 +0000

    adding changes to math module

---
+++ MathModule.py
@@ -0,0 +1,22 @@
+# Python Math Module from w3schools
+# Practice exercises
+# Author: Sheila Bambrick
+
+import math
+x = math.sqrt(64)
+print(x)
+
+
+# Calculation of maths ceiling / floor
+x = math.ceil(1.4)
+y = math.floor(1.4)
+
+print(x) 
+print(y) 
+
+
+# Maths PI
+
+x = math.pi
+
+print(x)
