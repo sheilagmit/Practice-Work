14c56c2ea30db852f338c4c4af3eabd3fe8c0586
Sheila Bambrick <G00398264@gmit.ie>
2021-02-09 20:36:38 +0000

    adding length programme

---
+++ len.py
@@ -0,0 +1,7 @@
+# This program reads in a String and outputs its length
+# From lab 3.3, Strings
+# Author: Sheila Bambrick
+
+inputString = input('enter a string:')
+lenghtOfString = len(inputString)
+print('The lenght of {} is {}'.format(inputString, lenghtOfString))
