abd2360e2893537907c9483b88fe18c2644332a1
Sheila Bambrick <G00398264@gmit.ie>
2021-02-09 07:37:02 +0000

    adding PythonMath.py

---
+++ pythonMath.py
@@ -0,0 +1,22 @@
+# Python Math from w3schools
+# Practice exercises
+# Author: Sheila Bambrick
+
+# Min & Max
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+
+
+# absolute
+x = abs(-7.25)
+
+print(x)
+
+
+# power function
+x = pow(4, 3)
+
+print(x)
